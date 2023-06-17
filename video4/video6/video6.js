6d74a10cb8965506bdc26e7ca6cf76ba3feaaff1
phxuan92 <phxuan92@gmail.com>
2023-06-18 02:03:58 +0700

    hoc ve number

---
+++ video4/video6/video6.js
@@ -0,0 +1,11 @@
+console.log(`hello world from html`.toUpperCase())
+let a=5;
+let b=6;
+let c=6.9;
+let d= `5`
+let e=+d
+
+console.log('e value: ',e, 'type of e', typeof e)
+console.log(a+b)
+console.log(a+c)
+console.log(a===e, 'a: ', typeof a,'e:', typeof e)
